c2865f03c0f089a263540d11b0710338afc40631
LuaScripterAnonymous <107802643+LuaScripterAnonymous@users.noreply.github.com>
2022-11-14 20:04:53 +0100

    Update VisualGUI.lua

--- VisualGUI.lua
+++ VisualGUI.lua
@@ -1189,7 +1189,7 @@ function Library:CreateWindow(HubName, GameName, IntroText, IntroIcon, ImprovePe
                 AnchorPoint = Vector2.new(0.5, 0.5),
                 Position = UDim2.new(0.5, 0, 0.5, 20),
                 ZIndex = 3,
-                Size = UDim2.new(0, 620, 0, 500),
+                Size = UDim2.new(0, 620, 0, 520),
                 Image = IntroIcon,
                 ScaleType = Enum.ScaleType.Fit
             })
